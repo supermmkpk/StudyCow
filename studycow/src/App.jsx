13039f54c8211d3034c47fed883884af70ecb7a1
박동민 <pmin1616@naver.com>
2024-07-23 16:57:21 +0900

    Fix: Navbar 에러 확인 후 해결

--- studycow/src/App.jsx
+++ studycow/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import StudyPage from "./views/StudyPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <StudyPage />
+    </>
+  );
 }
 
 export default App;
