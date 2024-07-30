6302fc59c5b9546b31351df5d27aa40b156ec2f9
노명환 <aud2531@naver.com>
2024-07-30 16:48:20 +0900

    Refactor : 성적 리스트 받아오기 refactor #S11P12C202-135

--- backend/src/main/java/com/studycow/web/ScoreController.java
+++ backend/src/main/java/com/studycow/web/ScoreController.java
@@ -49,20 +49,24 @@ public class ScoreController {
             List<ScoreDto> scoreList = scoreService.listScores(userId, subCode, myId);
             return ResponseEntity.ok(scoreList);
         } catch(Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
     @Operation(summary = "성적 단일 조회", description = "선택한 성적의 상세 정보를 조회합니다.")
-    @GetMapping("/detail")
-    public ResponseEntity<?> scoreDetail(@RequestParam("scoreId") Long scoreId) {
+    @GetMapping("/{userId}/detail/{scoreId}")
+    public ResponseEntity<?> scoreDetail(
+            @PathVariable int userId,
+            @PathVariable Long scoreId,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
         try {
-            ScoreDto scoreDto = scoreService.scoreDetail(scoreId);
+            int myId = userDetails.getUser().getUserId();
+            ScoreDto scoreDto = scoreService.scoreDetail(scoreId, userId, myId);
             return ResponseEntity.ok(scoreDto);
         } catch(Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("성적 조회 실패", HttpStatus.BAD_REQUEST);
+            //e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -78,8 +82,8 @@ public class ScoreController {
             return new ResponseEntity<>("성적 등록 성공", HttpStatus.OK);
 
         } catch(Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("성적 등록 실패", HttpStatus.BAD_REQUEST);
+            //e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
