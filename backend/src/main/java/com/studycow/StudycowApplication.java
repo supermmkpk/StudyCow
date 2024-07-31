899526df427da411f41d94e02a580fd443659296
kihun <chae0738@naver.com>
2024-08-01 01:09:32 +0900

    Feat: 운영서버 CORS 수정

--- backend/src/main/java/com/studycow/StudycowApplication.java
+++ backend/src/main/java/com/studycow/StudycowApplication.java
@@ -12,6 +12,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 //		}
 //)
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "https://i11c202.p.ssafy.io/studycow",description = "운영 서버")
+		}
+)
 public class StudycowApplication {
 
 	public static void main(String[] args) {
