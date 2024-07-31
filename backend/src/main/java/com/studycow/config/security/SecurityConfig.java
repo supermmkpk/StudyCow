d7f887f90fd7224f5ab9dc7c3fc712d7f3f41563
박동민 <pmin1616@naver.com>
2024-07-31 13:08:13 +0900

    Fix: CORS 수정

--- backend/src/main/java/com/studycow/config/security/SecurityConfig.java
+++ backend/src/main/java/com/studycow/config/security/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
