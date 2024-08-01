18e34e7dcecf3e42d3996669b47703b93e5073f0
SSAFY_NOTEBOOK <chae0738@naver.com>
2024-08-01 16:05:58 +0900

    Add: 로컬 환경 분리 #S11P12C202-61-실시간-채팅-api

--- backend/src/main/java/com/studycow/StudycowApplication.java
+++ backend/src/main/java/com/studycow/StudycowApplication.java
@@ -12,11 +12,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 //		}
 //)
 @SpringBootApplication
-@OpenAPIDefinition(
-		servers = {
-				@Server(url = "https://i11c202.p.ssafy.io/studycow",description = "운영 서버")
-		}
-)
 public class StudycowApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/studycow/config/SwaggerConfig.java
+++ backend/src/main/java/com/studycow/config/SwaggerConfig.java
@@ -1,42 +1,55 @@
 package com.studycow.config;
+
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
-//Swagger-UI 확인
-//http://localhost/swagger-ui.html
-
-@Configuration // 스프링 설정 클래스임을 나타냅니다.
+@Configuration
 public class SwaggerConfig {
+
+    @Value("${spring.profiles.active:unknown}")
+    private String activeProfile;
+
     @Bean
     public OpenAPI customOpenAPI() {
+        Server server;
+        if ("prod".equals(activeProfile)) {
+            server = new Server()
+                    .url("https://i11c202.p.ssafy.io/studycow")
+                    .description("운영 서버");
+        } else {
+            server = new Server()
+                    .url("http://localhost:8080/studycow")
+                    .description("개발 서버");
+        }
+
         return new OpenAPI()
+                .addServersItem(server)
                 .info(apiInfo())
                 .components(new Components()
-                .addSecuritySchemes("basicScheme",
-                        new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("basic"))
-                .addSecuritySchemes("bearerAuth",
-                new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT"))
+                        .addSecuritySchemes("basicScheme",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic"))
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT"))
                 ).addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 
-
     private Info apiInfo() {
-        return new Info().title("Study Cow API 문서") // API 제목을 설정합니다.
-                .description("<h3>REST API 문서 내용을 다음과 같이 제공합니다.</h3>") // API 설명을 설정합니다.
-                .version("1.0.0") // API 버전을 설정합니다.
+        return new Info().title("Study Cow API 문서")
+                .description("<h3>REST API 문서 내용을 다음과 같이 제공합니다.</h3>")
+                .version("1.0.0")
                 .license(new License().name("Apache 2.0").url("http://springdoc.org"));
     }
 }
