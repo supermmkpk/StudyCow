95b2bd05390ad7b5946a4aaded93c79bd200c8ca
박동민 <pmin1616@naver.com>
2024-08-05 17:31:50 +0900

    Add, Feat: 작성한 todo 개수 확인하는 기능 추가 #S11P12C202-88

--- studycow/src/components/MainPage/MainGrassItem.jsx
+++ studycow/src/components/MainPage/MainGrassItem.jsx
@@ -1,9 +1,55 @@
 import "./styles/MainGrassItem.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import useInfoStore from "../../stores/infos";
+import GrassCreate from "./GrassCreate";
+
+const API_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/studycow/";
 
 const MainGrassItem = () => {
+  const todayMonth = new Date().getMonth() + 1;
+  const todayYear = new Date().getFullYear();
+  const [thisMonthPlan, setThisMonthPlan] = useState([]);
+  const { token } = useInfoStore.getState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          API_URL + `planner/grass/${todayYear}/${todayMonth}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const planData = response.data.reduce((acc, item) => {
+          const day = item.planDate.split("-")[2]; // "2024-08-05"에서 일자 "05" 추출
+          acc[day] = item.planCount;
+          return acc;
+        }, {});
+        setThisMonthPlan(planData);
+      } catch (error) {
+        console.error("Error fetching the data", error);
+      }
+    };
+
+    fetchData();
+  }, [todayYear, todayMonth, token]);
+
   return (
     <div className="mainGrassContainer">
-      <h1>잔디&캐릭터</h1>
+      <div className="charImgPart">
+        <h1>캐릭터</h1>
+      </div>
+      <div className="createdGrass">
+        <GrassCreate
+          year={todayYear}
+          month={todayMonth}
+          plans={thisMonthPlan}
+        />
+      </div>
     </div>
   );
 };
--- studycow/src/components/MainPage/MainTodayTodo.jsx
+++ studycow/src/components/MainPage/MainTodayTodo.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./styles/MainTodayTodo.css";
 
+const sub_code_dic = {
+  1: "국어",
+  2: "수학",
+  3: "영어",
+  4: "한국사",
+  5: "사회탐구",
+  6: "과학탐구",
+  7: "직업탐구",
+  8: "제2외국어/한문",
+};
+
 const MainTodayTodo = ({ todayPlans }) => {
   return (
     <div className="mainTodayTodoList">
@@ -8,9 +19,12 @@ const MainTodayTodo = ({ todayPlans }) => {
         todayPlans.map((plan) => {
           const hours = Math.floor(plan.planStudyTime / 60);
           const minutes = plan.planStudyTime % 60;
+          const subjectName = sub_code_dic[plan.subCode];
           return (
             <div key={plan.planId} className="mainTodayTodoItem">
-              <p>{plan.planContent}</p>
+              <p>
+                {plan.planContent}({subjectName})
+              </p>
               <p>
                 {hours > 0 ? `${hours}시간 ` : ""}
                 {minutes}분
