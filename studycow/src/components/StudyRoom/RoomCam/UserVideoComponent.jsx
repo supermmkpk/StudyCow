c82bfc973dd7eaf4c02b9c47ff2187c6dc54fefc
sjyun9231@gmail.com <sjyun9231@gmail.com>
2024-08-08 08:52:20 +0900

    Refactor: 라우터 설정 수정

--- studycow/src/components/StudyRoom/RoomCam/UserVideoComponent.jsx
+++ studycow/src/components/StudyRoom/RoomCam/UserVideoComponent.jsx
@@ -3,17 +3,51 @@ import OpenViduVideoComponent from './OvVideo';
 import './UserVideo.css';
 
 export default class UserVideoComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            timer: 0
+        };
+        this.timerInterval = null;
+    }
+
+    componentDidMount() {
+        // 타이머 시작
+        this.timerInterval = setInterval(() => {
+            this.setState(prevState => ({
+                timer: prevState.timer + 1
+            }));
+        }, 1000); // 1초마다 타이머 업데이트
+    }
+
+    componentWillUnmount() {
+        // 컴포넌트가 언마운트 될 때 타이머 정리
+        if (this.timerInterval) {
+            clearInterval(this.timerInterval);
+        }
+    }
+
     getNicknameTag() {
         return JSON.parse(this.props.streamManager.stream.connection.data).clientData;
     }
 
+    formatTime(seconds) {
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const secs = seconds % 60;
+        return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+    }
+
     render() {
         return (
-            <div className="container">
+            <div className="streamingContainer">
                 {this.props.streamManager !== undefined ? (
                     <div className="streamcomponent">
                         <OpenViduVideoComponent streamManager={this.props.streamManager} />
-                        <div><p>{this.getNicknameTag()}</p></div>
+                        <div className='streamInfoContainer'>
+                            <p className='streamName'>{this.getNicknameTag()}</p>
+                            <p className='streamTime'>{this.formatTime(this.state.timer)}</p>
+                        </div>
                     </div>
                 ) : null}
             </div>
