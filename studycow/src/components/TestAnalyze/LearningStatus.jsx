680ea8d1c66e54aea4eee1f92e9c5ea71cb40c18
김정환 <jeonghwan916@gmail.com>
2024-08-07 14:17:37 +0900

    FIX: 성적 현황정보 연결 #S11P12C202-105

--- studycow/src/components/TestAnalyze/LearningStatus.jsx
+++ studycow/src/components/TestAnalyze/LearningStatus.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect } from "react";
 import useLearningStatusStore from "../../stores/learningStatus"; // 학습 상태 관리를 위한 zustand 스토어
 import useInfoStore from "../../stores/infos"; // 사용자 정보 및 로그인 상태 관리를 위한 zustand 스토어
+import useGradeStore from "../../stores/grade"; // 과목 선택 상태 관리를 위한 zustand 스토어
 import "./styles/LearningStatus.css"; // CSS 파일 가져오기
 
-const LearningStatus = ({ subCode }) => {
+const LearningStatus = () => {
   // 학습 상태 스토어에서 상태와 함수를 가져오기
   const { subjectInfo, fetchSubjectInfo, error } = useLearningStatusStore();
 
   // 사용자 정보 스토어에서 로그인 상태와 사용자 정보를 가져오기
   const { isLogin, userInfo } = useInfoStore();
 
+  // 과목 선택 스토어에서 selectedSubject 가져오기
+  const { selectedSubject } = useGradeStore();
+
   // 컴포넌트가 마운트될 때 학습 상태 데이터를 가져오기 위해 useEffect 사용
   useEffect(() => {
-    if (isLogin && userInfo.userId !== 0 && subCode) {
-      // 로그인 상태일 때만 데이터 가져오기
-      fetchSubjectInfo(subCode);
+    if (isLogin && userInfo.userId !== 0 && selectedSubject) {
+      // 로그인 상태 및 선택된 과목이 있을 때만 데이터 가져오기
+      fetchSubjectInfo(selectedSubject);
     }
-  }, [fetchSubjectInfo, subCode, isLogin, userInfo.userId]);
+  }, [fetchSubjectInfo, selectedSubject, isLogin, userInfo.userId]);
 
   return (
     <div className="learning-status-container">
@@ -34,7 +38,8 @@ const LearningStatus = ({ subCode }) => {
           <div className="learning-status-row">
             <span className="learning-status-label">플래너 계획 시간</span>
             <span className="learning-status-value">
-              시간 {/* 추가할 데이터 */}
+              {/* 추가할 데이터 */}
+              {subjectInfo.plannerTime} 시간
             </span>
           </div>
           <div className="learning-status-row">
