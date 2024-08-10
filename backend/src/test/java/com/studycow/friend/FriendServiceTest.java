163ec6cc84b4ae7d4ea6d66002a8c8994664bafb
Bongkyoon <supermmkpk@gmail.com>
2024-08-10 18:33:38 +0900

    Feat: 파일 관리 테스트 코드 완성 #S11P12C202-196 #done

--- backend/src/test/java/com/studycow/friend/FriendServiceTest.java
+++ backend/src/test/java/com/studycow/friend/FriendServiceTest.java
@@ -16,11 +16,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+/**
+ * 친구 관리 테스트 클래스
+ *
+ * @author 박봉균
+ * @since JDK17(Eclipse Temurin)
+ */
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
@@ -37,9 +43,7 @@ public class FriendServiceTest {
     @Autowired
     private UserGradeRepository userGradeRepository;
 
-    private int userId1;
-    private int userId2;
-    private int userId3;
+    private List<Integer> userIdList = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
@@ -53,109 +57,135 @@ public class FriendServiceTest {
         registerRequestDto.setUserPassword("testPass");
         registerRequestDto.setUserNickName("testNick");
         registerRequestDto.setUserPublic(1);
-        userId1 = userService.register(registerRequestDto).getUserId();
+        userIdList.add(userService.register(registerRequestDto).getUserId());
 
         registerRequestDto = new RegisterRequestDto();
         registerRequestDto.setUserEmail("test2@mail.com");
         registerRequestDto.setUserPassword("testPass2");
         registerRequestDto.setUserNickName("testNick2");
         registerRequestDto.setUserPublic(1);
-        userId2 = userService.register(registerRequestDto).getUserId();
+        userIdList.add(userService.register(registerRequestDto).getUserId());
 
         registerRequestDto = new RegisterRequestDto();
         registerRequestDto.setUserEmail("test3@mail.com");
         registerRequestDto.setUserPassword("testPass3");
         registerRequestDto.setUserNickName("testNick3");
         registerRequestDto.setUserPublic(1);
-        userId3 = userService.register(registerRequestDto).getUserId();
+        userIdList.add(userService.register(registerRequestDto).getUserId());
     }
 
+
+    /**
+     * 친구 요청 전송 테스트
+     *
+     * @throws Exception
+     */
     @Test
     void testSendFriendRequest() throws Exception {
-        // Given
-        int fromUserId = userId1;
-        int toUserId = userId2;
+        // GIVEN
+        int fromUserId = userIdList.get(0);
+        int toUserId = userIdList.get(1);
 
-        // When
+        // WHEN
         friendService.saveFriendRequest(fromUserId, toUserId);
 
-        // Then
+        // THEN
         List<FriendRequestDto> sentRequests = friendService.listFriendRequestSent(fromUserId, new ListOptionDto());
         assertTrue(sentRequests.stream().anyMatch(request -> request.getCounterpartUserId() == toUserId));
     }
 
+    /**
+     * 보낸 친구 요청 목록 조회 테스트
+     *
+     * @throws Exception
+     */
     @Test
     void testListFriendRequestSent() throws Exception {
-        // Given
-        int fromUserId = userId1;
-        int toUserId = userId2;
+        // GIVEN
+        int fromUserId = userIdList.get(0);
+        int toUserId = userIdList.get(1);
         ListOptionDto listOptionDto = new ListOptionDto();
         listOptionDto.setSearchText("test");
 
-        // When
+        // WHEN
         friendService.saveFriendRequest(fromUserId, toUserId);
         List<FriendRequestDto> result = friendService.listFriendRequestSent(fromUserId, listOptionDto);
 
-        // Then
+        // THEN
         assertNotNull(result);
         assertFalse(result.isEmpty());
         assertTrue(result.stream().allMatch(request -> request.getCounterpartUserNickname().contains("test")));
     }
 
+    /**
+     * 받은 친구 요청 목록 조회 테스트
+     *
+     * @throws Exception
+     */
     @Test
     void testListFriendRequestReceived() throws Exception {
-        // Given
-        int fromUserId = userId1;
-        int toUserId = userId2;
+        // GIVEN
+        int fromUserId = userIdList.get(0);
+        int toUserId = userIdList.get(1);
         ListOptionDto listOptionDto = new ListOptionDto();
         listOptionDto.setSearchText("test");
 
-        // When
+        // WHEN
         friendService.saveFriendRequest(fromUserId, toUserId);
         List<FriendRequestDto> result = friendService.listFriendRequestReceived(toUserId, listOptionDto);
 
-        // Then
+        // THEN
         assertNotNull(result);
         assertFalse(result.isEmpty());
         assertTrue(result.stream().allMatch(request -> request.getCounterpartUserNickname().contains("test")));
     }
 
+    /**
+     * 친구 요청 승인 테스트
+     *
+     * @throws Exception
+     */
     @Test
     void testAcceptFriendRequest() throws Exception {
-        // Given
+        // GIVEN
         int friendRequestId = 1;
-        int fromUserId = userId1;
-        int toUserId = userId2;
+        int fromUserId = userIdList.get(0);
+        int toUserId = userIdList.get(1);
 
-        // When
+        // WHEN
         friendService.saveFriendRequest(fromUserId, toUserId);
         friendService.acceptFriendRequest(friendService.recentFriendRequestId(), toUserId);
 
-        // Then
+        // THEN
         assertFalse(() -> friendRepository.existsFriendRequestById(friendRequestId));
         List<FriendDto> friends = friendRepository.listFriends(fromUserId, new ListOptionDto());
         assertTrue(friends.stream().anyMatch(friend -> friend.getFriendUserId() == toUserId));
     }
 
+    /**
+     * 친구 목록 조회 테스트
+     *
+     * @throws Exception
+     */
     @Test
     void testListFriends() throws Exception {
-        // Given
-        int fromUserId = userId1;
-        int toUserId1 = userId2;
-        int toUserId2 = userId3;
-        int userId = userId1;
+        // GIVEN
+        int fromUserId = userIdList.get(0);
+        int toUserId1 = userIdList.get(1);
+        int toUserId2 = userIdList.get(2);
+        int userId = userIdList.get(0);
         ListOptionDto listOptionDto = new ListOptionDto();
         listOptionDto.setSearchText("test");
         listOptionDto.setIsDESC(true);
 
-        // When
+        // WHEN
         friendService.saveFriendRequest(fromUserId, toUserId1);
         friendService.acceptFriendRequest(friendService.recentFriendRequestId(), toUserId1);
         friendService.saveFriendRequest(fromUserId, toUserId2);
         friendService.acceptFriendRequest(friendService.recentFriendRequestId(), toUserId2);
         List<FriendDto> result = friendService.listFriends(userId, listOptionDto);
 
-        // Then
+        // THEN
         assertNotNull(result);
         assertFalse(result.isEmpty());
         assertTrue(result.stream().allMatch(friendDto -> friendDto.getFriendNickname().contains("test")));
@@ -163,35 +193,45 @@ public class FriendServiceTest {
         assertTrue(result.get(0).getFriendDate().isAfter(result.get(1).getFriendDate()));
     }
 
+    /**
+     * 친구 해제 테스트
+     *
+     * @throws Exception
+     */
     @Test
     void testCancelFriend() throws Exception {
-        // Given
-        int fromUserId = userId1;
-        int toUserId = userId2;
-        int userId = userId1;
+        // GIVEN
+        int fromUserId = userIdList.get(0);
+        int toUserId = userIdList.get(1);
+        int userId = userIdList.get(0);
 
-        // When
+        // WHEN
         friendService.saveFriendRequest(fromUserId, toUserId);
         friendService.acceptFriendRequest(friendService.recentFriendRequestId(), toUserId);
         friendService.deleteFriend(toUserId, userId);
 
-        // Then
+        // THEN
         List<FriendDto> friends = friendRepository.listFriends(userId, new ListOptionDto());
         assertTrue(friends.stream().noneMatch(friend -> friend.getFriendUserId() == toUserId));
     }
 
+    /**
+     * 친구 요청 삭제 테스트
+     *
+     * @throws Exception
+     */
     @Test
     void testCancelFriendRequest() throws Exception {
-        // Given
-        int fromUserId = userId1;
-        int toUserId = userId2;
+        // GIVEN
+        int fromUserId = userIdList.get(0);
+        int toUserId = userIdList.get(1);
         int friendRequestId = 1;
 
-        // When
+        // WHEN
         friendService.saveFriendRequest(fromUserId, toUserId);
         friendService.deleteFriendRequest(friendService.recentFriendRequestId());
 
-        // Then
+        // THEN
         assertFalse(friendRepository.existsFriendRequestById(friendRequestId));
     }
 }
