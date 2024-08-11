cb76816c8e073eead58c8ae7bb6c8faa1aeb918b
박동민 <pmin1616@naver.com>
2024-08-11 18:01:00 +0900

    Design: 메인페이지, 성적 분석 페이지에도 Navbar 크기에 따라 margin-top 적용되도록 수정 #S11P12C202-105

--- studycow/src/views/AnalyzePage.jsx
+++ studycow/src/views/AnalyzePage.jsx
@@ -1,12 +1,31 @@
 import "../styles/AnalyzePage.css";
 import Navbar from "../components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import { useRef, useEffect } from "react";
 
 const AnalyzePage = () => {
+  const analyzeMainRef = useRef(null);
+
+  useEffect(() => {
+    const updateAnalyzeMainMargin = () => {
+      const navHeight = getComputedStyle(
+        document.documentElement
+      ).getPropertyValue("--nav-height");
+      if (analyzeMainRef.current) {
+        analyzeMainRef.current.style.marginTop = navHeight;
+      }
+    };
+
+    updateAnalyzeMainMargin();
+    window.addEventListener("resize", updateAnalyzeMainMargin);
+
+    return () => window.removeEventListener("resize", updateAnalyzeMainMargin);
+  }, []);
+
   return (
     <>
       <Navbar />
-      <div className="analyzeMain">
+      <div ref={analyzeMainRef} className="analyzeMain">
         <Outlet />
       </div>
     </>
