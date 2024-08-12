7eccd7a7e6e1d2763c84801193172006d90f37c1
SSAFY_NOTEBOOK <chae0738@naver.com>
2024-08-12 14:04:11 +0900

    Feat: Dto 검증 추가

--- backend/src/main/java/com/studycow/dto/plan/PlannerCreateDto.java
+++ backend/src/main/java/com/studycow/dto/plan/PlannerCreateDto.java
@@ -2,9 +2,11 @@ package com.studycow.dto.plan;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 import java.time.LocalDate;
 
@@ -17,11 +19,13 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PlannerCreateDto {
+
     /** 과목 코드 */
     private int subCode;
     /** 플랜 날짜 */
     private LocalDate planDate;
     /** 내용 */
+    @Size(min = 0, max = 100, message = "글자 수는 0 ~ 100 자 이내여야 합니다.")
     private String planContent;
     /** 계획 학습 시간 */
     private int planStudyTime;
