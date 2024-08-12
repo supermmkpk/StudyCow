7b6ff7690d4767f316b15606d74744c0f3066b7b
homedesktop <sjyun9231@gmail.com>
2024-08-12 19:46:22 +0900

    Design: 메인 비로그인 페이지 비율 수정

--- studycow/src/components/MainPage/Main_Unlogin.jsx
+++ studycow/src/components/MainPage/Main_Unlogin.jsx
@@ -16,7 +16,7 @@ const Main_Unlogin = () => {
           alt="First slide"
           text="당신의 캐릭터를, 성장시키는 즐거움"
           left="40%"
-          top="400px"
+          top="calc(400/1440*100vw)"
         />
       </Carousel.Item>
       <Carousel.Item>
@@ -25,7 +25,7 @@ const Main_Unlogin = () => {
           alt="Second slide"
           text="해야 하는 todo, check-list로 확인"
           left="100%"
-          top="400px"
+          top="calc(400/1440*100vw)"
         />
       </Carousel.Item>
       <Carousel.Item>
@@ -34,7 +34,7 @@ const Main_Unlogin = () => {
           alt="Third slide"
           text="성적 분석 기능을 통한, 나의 성장 확인 가능"
           left="90%"
-          top="150px"
+          top="calc(150/1440*100vw)"
         />
       </Carousel.Item>
       <Carousel.Item>
@@ -43,7 +43,7 @@ const Main_Unlogin = () => {
           alt="Forth slide"
           text="Study with me!, 온라인으로 함께하는 공부, 친구들과의 시간 경쟁"
           left="40%"
-          top="150px"
+          top="calc(150/1440*100vw)"
         />
       </Carousel.Item>
     </Carousel>
