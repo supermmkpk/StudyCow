1ab7d2fa58599a3776b1616d9a984e0f49fbd7cd
박동민 <pmin1616@naver.com>
2024-08-12 17:52:35 +0900

    Feat: 친구 Id를 기준으로 닉네임 출력 기능까지 구현 #S11P12C202-105

--- studycow/src/components/FriendGrade/FriendGradeMain.jsx
+++ studycow/src/components/FriendGrade/FriendGradeMain.jsx
@@ -1,9 +1,62 @@
 import "./styles/FriendGradeMain.css";
+import { useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
+import useGradeStore from "../../stores/grade";
+import useSubjectStore from "../../stores/subjectStore";
+import useFriendsStore from "../../stores/friends";
 
 const FriendGradeMain = () => {
+  const { userId } = useOutletContext();
+  const { selectedSubject, setSelectedSubject } = useGradeStore();
+  const { subjects, fetchSubjects } = useSubjectStore();
+  const { friendInfo } = useFriendsStore();
+
+  useEffect(() => {
+    fetchSubjects();
+  }, [fetchSubjects]);
+
+  const handleSubjectChange = (subjectCode) => {
+    if (selectedSubject === subjectCode) {
+      setSelectedSubject("");
+      setTimeout(() => setSelectedSubject(subjectCode), 0);
+    } else {
+      setSelectedSubject(subjectCode);
+    }
+    console.log(subjectCode);
+  };
+
   return (
-    <div>
-      <h1>안녕?</h1>
+    <div className="friendGradeContainer">
+      <div className="friendGradeHeader">
+        <h1>{friendInfo.userNickName}님 어서오세요</h1>
+        <div className="friendGradeSideNav">
+          <div className="friendGradeSubjectSelect">
+            <select
+              name="subject"
+              onChange={(e) => handleSubjectChange(e.target.value)}
+              value={selectedSubject}
+              className="form-control ml-2"
+            >
+              <option value="" disabled hidden>
+                과목 선택
+              </option>
+              {subjects.map((subject) => (
+                <option key={subject.subCode} value={subject.subCode}>
+                  {subject.subName}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="friendGradeBody">
+        <div className="friendCowStatus">
+          <h1>캐릭터 부분</h1>
+        </div>
+        <div className="friendGradeStatus">
+          <h1>성적 분석 부분</h1>
+        </div>
+      </div>
     </div>
   );
 };
