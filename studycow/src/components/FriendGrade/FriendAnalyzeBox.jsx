3293d10b3b3eeacd60b798f51ea0ab87a1803e71
박동민 <pmin1616@naver.com>
2024-08-12 22:46:51 +0900

    Add, Feat: 친구의 취약점 Top 3 출력기능 추가 #S11P12C202-104

--- studycow/src/components/FriendGrade/FriendAnalyzeBox.jsx
+++ studycow/src/components/FriendGrade/FriendAnalyzeBox.jsx
@@ -1,12 +1,13 @@
-import './styles/FriendAnalyzeBox.css'
-import FriendRecentGrades from './FriendRecentGrades'
-import FriendLearningStatus from './FriendLearningStatus'
+import "./styles/FriendAnalyzeBox.css";
+import FriendRecentGrades from "./FriendRecentGrades";
+import FriendLearningStatus from "./FriendLearningStatus";
+import FriendWeakBlock from "./FriendWeakBlock";
 
-const FriendAnalyzeBox = ({userId}) => {
+const FriendAnalyzeBox = ({ userId }) => {
   return (
-    <div className='selectedSubjectContainer'>
-      <div className='upsideContainer'>
-      <div className="recentGradesGraph">
+    <div className="selectedSubjectContainer">
+      <div className="upsideContainer">
+        <div className="recentGradesGraph">
           <h3>최근 성적 변화 그래프</h3>
           <div>
             <FriendRecentGrades userId={userId} />
@@ -14,9 +15,11 @@ const FriendAnalyzeBox = ({userId}) => {
         </div>
         <FriendLearningStatus userId={userId} />
       </div>
-      <div className='downsideContainer'></div>
+      <div className="downsideContainer">
+        <FriendWeakBlock userId={userId} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FriendAnalyzeBox
+export default FriendAnalyzeBox;
