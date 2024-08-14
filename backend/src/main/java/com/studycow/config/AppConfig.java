8793720b70f06122085da9b58032361b80fb71bb
SSAFY_NOTEBOOK <chae0738@naver.com>
2024-08-14 11:34:03 +0900

    Delete: record 삭제

--- backend/src/main/java/com/studycow/config/AppConfig.java
+++ backend/src/main/java/com/studycow/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.studycow.config;
 
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.studycow.domain.UserSubjectPlan;
+import com.studycow.dto.plan.PlannerGetDto;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.module.jsr310.Jsr310Module;
@@ -22,7 +24,27 @@ public class AppConfig {
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.registerModule(new Jsr310Module());
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+
+        // PlannerGetDto에 대한 명시적 매핑 추가
+        modelMapper.createTypeMap(UserSubjectPlan.class, PlannerGetDto.class)
+                .setConverter(context -> {
+                    UserSubjectPlan source = context.getSource();
+                    return new PlannerGetDto(
+                            Integer.parseInt(String.valueOf(source.getPlanId())),
+                            source.getUser().getId(),
+                            Integer.parseInt(String.valueOf(source.getSubCode())),
+                            source.getPlanDate(),
+                            source.getPlanContent(),
+                            source.getPlanStudyTime(),
+                            source.getPlanStatus()
+                    );
+                });
         return modelMapper;
     }
+
+
 
 }
