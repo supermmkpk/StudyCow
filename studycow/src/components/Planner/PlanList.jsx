ea1a369d10f0479274ba6faee5b4170063a1481b
김정환 <jeonghwan916@gmail.com>
2024-08-14 10:05:56 +0900

    Fix: ai 시간 입력 개선  #S11P12C202-99

--- studycow/src/components/Planner/PlanList.jsx
+++ studycow/src/components/Planner/PlanList.jsx
@@ -6,6 +6,7 @@ import usePlanStore from "../../stores/plan";
 import useSubjectStore from "../../stores/subjectStore"; // subject store import
 import PlanModify from "./CreateModify/PlanModify"; // PlanModify 모달 컴포넌트
 import Notiflix from "notiflix";
+import { Confirm } from "notiflix"; // Notiflix import 추가
 
 const PlanList = () => {
   const { plans, changePlanStatus, deletePlan, setPlans } = usePlanStore(); // changePlanStatus 가져오기
@@ -43,8 +44,20 @@ const PlanList = () => {
 
   // 삭제 핸들러 함수
   const handleDeleteClick = async (planId) => {
+    Confirm.init({
+      titleColor: "#ff5549", // 빨간색 (빨간색은 #ff5549, 초록색은 #008000)
+      okButtonBackground: "#ff5549", // 빨간색
+      cancelButtonBackground: "#a9a9a9", // 회색
+      titleFontSize: "20px", // 제목 폰트 크기 증가
+      width: "300px", // 대화상자 너비 설정
+      messageColor: "#1e1e1e", // 메시지 색상 설정
+      messageFontSize: "16px", // 메시지 폰트 크기 설정
+      buttonsFontSize: "14px", // 버튼 폰트 크기 설정
+      borderRadius: "20px",
+    });
+
     Notiflix.Confirm.show(
-      "삭제 확인",
+      "플랜 삭제",
       "정말로 삭제하시겠습니까?",
       "예",
       "아니오",
@@ -52,17 +65,17 @@ const PlanList = () => {
         try {
           const success = await deletePlan(planId);
           if (success) {
-            Notiflix.Notify.success("플래너가 성공적으로 삭제되었습니다.");
+            Notiflix.Notify.success("플랜이 성공적으로 삭제되었습니다.");
             setPlans(plans.filter((plan) => plan.planId !== planId)); // 상태 갱신
           } else {
-            Notiflix.Notify.failure("플래너 삭제에 실패했습니다.");
+            Notiflix.Notify.failure("플랜 삭제에 실패했습니다.");
           }
         } catch (error) {
-          Notiflix.Notify.failure("플래너 삭제에 실패했습니다.");
+          Notiflix.Notify.failure("플랜 삭제에 실패했습니다.");
         }
       },
       () => {
-        Notiflix.Notify.info("플래너 삭제가 취소되었습니다.");
+        Notiflix.Notify.info("플랜 삭제가 취소되었습니다.");
       }
     );
   };
